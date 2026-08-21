a2f844409d59e9052751fd2068a301e678569a41
param19304-lang <param19304@gmail.com>
2026-08-21 14:07:05 +0530

    Update NavGraph.kt

--- app/src/main/java/com/example/rentmanagement/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/rentmanagement/ui/navigation/NavGraph.kt
@@ -1,5 +1,6 @@
 package com.example.rentmanagement.ui.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
